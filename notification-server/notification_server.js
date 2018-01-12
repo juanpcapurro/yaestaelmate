e26c7c38be0a20a24dbe8dd10c66228627eef502
capurro <juanpcapurro@gmail.com>
2018-01-12 18:40:55 -0300

    added barebones notification server

---
+++ notification-server/notification_server.js
@@ -0,0 +1,23 @@
+var jayson  = require('jayson');
+const child_process = require('child_process');
+var cors = require('cors');
+var connect = require('connect');
+var jsonParser = require('body-parser').json;
+var app = connect();
+
+var server = jayson.server({
+  ya_esta_el_mate: function(args, callback) {
+    if (typeof args[0] !== 'string'){
+      child_process.spawn('notify-send', ['ya esta el mate']);
+    }else{
+      child_process.spawn('notify-send', [args[0]]);
+    }
+    callback(null,);
+  }
+});
+
+app.use(cors({methods: ['POST']}));
+app.use(jsonParser());
+app.use(server.middleware());
+
+app.listen(3000);
