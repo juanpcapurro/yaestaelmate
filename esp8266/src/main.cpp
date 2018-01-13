66c2975fde69e6e109b4d7d3b03703b7f749343c
capurro <juanpcapurro@gmail.com>
2018-01-12 21:14:43 -0300

    minimal notificator without buttons

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,94 +1,30 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
-#include <FS.h>
+#include <ESP8266HTTPClient.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>
 
-MDNSResponder mdns;
-ESP8266WebServer server(80);
+#include "pinout.h"
 
-void handle_index() {
-  Serial.println("handling index");
-  File file = SPIFFS.open("/index.html","r");
-  server.streamFile(file, "text/html");
-  file.close();
-}
-
-void handleNotFound(){
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
-
-void handle_get_ssid_and_password(){
-  if(!server.hasArg("ssid") || !server.hasArg("password"))
-    Serial.println("incorrect argument format!");
-  const char* SSID = server.arg("ssid").c_str();
-  const char* password = server.arg("password").c_str();
-  Serial.print("SSID: ");
-  Serial.println(SSID);
-  Serial.print("password: ");
-  Serial.println(password);
-  WiFi.softAPdisconnect();
-  delay(200);
-  WiFi.begin(SSID,password);
-  for(int i=0; i<5 && WiFi.status() != WL_CONNECTED; i++){
-    delay(1000);
-  }
-
-  if(WiFi.status() == WL_CONNECTED){
-    Serial.print("Connected to ");
-    Serial.println(SSID);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
-    if (mdns.begin("esp8266", WiFi.localIP())) {
-      Serial.println("MDNS responder started");
-    }
-    server.stop();
-    server.close();
-    server.onNotFound([]{
-      server.send(418, "text/plain", "I'm a teapot.\nWell, not actually a teapot, I'm a mate.\nAnd I hold no affiliation with the desktop environment.");
-    });
-    server.begin();
-    Serial.println("HTTP server restarted");
-  }
-  else{
-    Serial.println("Invlid SSID/password. Check for typos");
-    server.send(200,"text/plain","Invlid SSID/password. Check for typos");
-  }
-}
+const char* NOTIFICATION_SERVER_URL = "http://192.168.0.17:3000/json_rpc";
 
 void setup(void){
-  Serial.begin(115200);
-  SPIFFS.begin();
-  WiFi.mode(WIFI_AP);
+    WiFiManager wifiManager;
+    Serial.begin(115200);
+    wifiManager.autoConnect("gaucho");
+    Serial.print("Connected succesfully. IP: ");
+    Serial.println(WiFi.localIP());
 
-  WiFi.softAPConfig(IPAddress(192,168,0,1),IPAddress(192,168,0,1),IPAddress(255,255,255,0));
-  const char* ssid = "yaestaelmate";
-  WiFi.softAP(ssid);
-  Serial.print(WiFi.softAPIP());
-  Serial.println(" Is the local IP. Waiting for configuration");
-  WiFi.printDiag(Serial);
-  Serial.println("");
-  
-  server.on("/", handle_index);
-  server.on("/get_ssid_and_password", handle_get_ssid_and_password);
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-  Serial.println("HTTP server started");
+    while (true){
+        HTTPClient http;
+        http.begin(NOTIFICATION_SERVER_URL);
+        http.addHeader("Content-Type","application/json");
+        int http_code = http.POST("{\"jsonrpc\": \"2.0\", \"method\": \"ya_esta_el_mate\", \"params\": [\"ya esta el mate\"]}");
+        Serial.printf("POSTed. return value: %i\n",http_code);
+        delay(5000);
+    }
 }
 
 void loop(void){
-  server.handleClient();
 }
---
+++ esp8266/src/pinout.h
@@ -0,0 +1,18 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+#define D0 16
+#define D1 5
+#define D2 4
+#define D3 0
+#define D4 2
+#define D5 14
+#define D6 12
+#define D7 13
+#define D8 15
+#define RX 3
+#define TX 1
+#define SD2 9
+#define SD3 10
+
+#endif
